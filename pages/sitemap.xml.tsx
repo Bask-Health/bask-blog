66fe3c29b9576b8ccec8a120bb25d14bb2fb242e
Zachary Dorf <88392270+inveve@users.noreply.github.com>
2024-10-25 15:35:59 -0400

    Update sitemap (#11)
    * test sitemap updating dynamic dates
    
    * Update sitemap.xml.tsx

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,5 +1,4 @@
 import type { GetServerSideProps } from 'next'
-
 import { host } from '@/lib/config'
 import { getSiteMap } from '@/lib/get-site-map'
 import type { SiteMap } from '@/lib/types'
@@ -31,11 +30,14 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   }
 }
 
-const createSitemap = (siteMap: SiteMap) =>
-  `<?xml version="1.0" encoding="UTF-8"?>
+const createSitemap = (siteMap: SiteMap) => {
+  const currentDate = new Date().toISOString()
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
       <loc>${host}/blog</loc>
+      <changefreq>daily</changefreq>
     </url>
 
     <url>
@@ -43,15 +45,19 @@ const createSitemap = (siteMap: SiteMap) =>
     </url>
 
     ${Object.keys(siteMap.canonicalPageMap)
-      .map((canonicalPagePath) =>
-        `
+      .map((canonicalPagePath) => {
+        const lastMod = siteMap.pageModDates[canonicalPagePath] || currentDate
+        return `
           <url>
             <loc>${host}/blog/${canonicalPagePath}</loc>
+            <lastmod>${lastMod}</lastmod>
           </url>
         `.trim()
-      )
+      })
       .join('')}
-  </urlset>
-`
+  </urlset>`
+}
 
-export default () => null
+export default function Sitemap() {
+  return null
+}
